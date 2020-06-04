737767597e735fb6b292ccd51edd4c11259ba35d
ChipCE <login721@gmail.com>
2020-06-04 12:41:20 +0900

    updated config generator

--- js/gero.js
+++ js/gero.js
@@ -70,14 +70,14 @@ function make()
             console.log(value);
             let macro = $("#mainForceMacro").val();
 
-            configStr = '<!-- @' + name + ' --><div class="config-option-warpper col-12"><label for="' + name + '" class="text-primary"><i class="fa fa-chevron-down"></i> ' + diplayName + '</label><input type="' + type + '" class="form-control config-option" id="' + name + '" placeholder="" name="' + name + '" ' + ' autocomplete="off" value="' + value + '"' + ((require == "true")?' required="true"':'') + ' forceMacro="'+ macro + '"><p class="text-muted"><i class="fa fa-info-circle"></i> ' + description + '</p></div>';
+            configStr = '<!-- @' + name + ' -->\n<div class="config-option-warpper col-12">\n\t<label for="' + name + '" class="text-primary"><i class="fa fa-chevron-down"></i> ' + diplayName + '</label>\n\t<input type="' + type + '" class="form-control config-option" id="' + name + '" placeholder="" name="' + name + '" ' + ' autocomplete="off" value="' + value + '"' + ((require == "true")?' required="true"':'') + ' forceMacro="'+ macro + '">\n\t<p class="text-muted"><i class="fa fa-info-circle"></i> ' + description + '</p>\n</div>';
             $("#source").text(configStr);
         }
         else if(type=="checkbox")
         {
             let value = $("#mainDefaultValue").val();
             console.log(value);
-            let configStr = '<!-- @' + name + ' --><div class="config-option-warpper col-12"><input class="form-check-input config-option" type="checkbox" value="" id="' + name + '" name="' + name + '" autocomplete="off"' + ((value=="true")?' checked="checked"':'') +'><label class="form-check-label text-primary" for="' + name + '">' + diplayName + '</label><p class="text-muted"><i class="fa fa-info-circle"></i> ' + description + '</p></div>';
+            let configStr = '<!-- @' + name + ' -->\n<div class="config-option-warpper col-12">\n\t<input class="form-check-input config-option" type="checkbox" value="" id="' + name + '" name="' + name + '" autocomplete="off"' + ((value=="true")?' checked="checked"':'') +'>\n\t<label class="form-check-label text-primary" for="' + name + '">' + diplayName + '</label>\n\t<p class="text-muted"><i class="fa fa-info-circle"></i> ' + description + '</p>\n</div>';
             $("#source").text(configStr);
         }
         else if(type=="select")
@@ -86,13 +86,13 @@ function make()
             let arg = $("#mainConfigArg").val();
             let argList=arg.split(",");
             console.log(argList);
-            let configStr='<!-- @' + name + ' --><div class="config-option-warpper col-12"><label for="' + name + '" class="text-primary"><i class="fa fa-chevron-down"></i> ' + diplayName + '</label><select class="custom-select config-option" name="' + name + '" autocomplete="off" id="' + name + '" appendId="' + appendId + '">';
+            let configStr='<!-- @' + name + ' -->\n<div class="config-option-warpper col-12">\n\t<label for="' + name + '" class="text-primary"><i class="fa fa-chevron-down"></i> ' + diplayName + '</label>\n\t<select class="custom-select config-option" name="' + name + '" autocomplete="off" id="' + name + '" appendId="' + appendId + '">\n';
             for(let i = 0;i<argList.length;i++)
             {
                 
-                configStr = configStr + '<option value="' + argList[i] + '"' + ((i==0)?' selected="selected"':'') +'>' + argList[i] +'</option>';
+                configStr = configStr + '\t\t<option value="' + argList[i] + '"' + ((i==0)?' selected="selected"':'') +'>' + argList[i] +'</option>\n';
             }
-            configStr = configStr + '</select><p class="text-muted"><i class="fa fa-info-circle"></i> ' + description + '</p></div>';
+            configStr = configStr + '\t</select>\n\t<p class="text-muted"><i class="fa fa-info-circle"></i> ' + description + '</p>\n</div>';
             $("#source").text(configStr);
         }
     }
@@ -120,14 +120,14 @@ function make()
             let value = $("#subDefaultValue").val();
             console.log(value);
 
-            configStr = '<!-- @' + name + ' --><div class="config-option-warpper-sub col-12 d-none" parent="' + targetConfig + '" targetValue="' + targetValue + '"><label for="' + name + '" class="text-primary"><i class="fa fa-chevron-down"></i> ' + diplayName + '</label><input type="' + type + '" class="form-control config-option-sub" id="' + name + '" placeholder="" name="' + name + '" ' + ' autocomplete="off" value="' + value + '"' + ((require == "true")?' required="true"':'') + ' forceMacro="' + macro +'"><p class="text-muted"><i class="fa fa-info-circle"></i> ' + description + '</p></div>';
+            configStr = '<!-- @' + name + ' -->\n<div class="config-option-warpper-sub col-12 d-none" parent="' + targetConfig + '" targetValue="' + targetValue + '">\n\t<label for="' + name + '" class="text-primary"><i class="fa fa-chevron-down"></i> ' + diplayName + '</label>\n\t<input type="' + type + '" class="form-control config-option-sub" id="' + name + '" placeholder="" name="' + name + '" ' + ' autocomplete="off" value="' + value + '"' + ((require == "true")?' required="true"':'') + ' forceMacro="' + macro +'">\n\t<p class="text-muted"><i class="fa fa-info-circle"></i> ' + description + '</p>\n</div>';
             $("#source").text(configStr);
         }
         else if(type=="checkbox")
         {
             let value = $("#subDefaultValue").val();
             console.log(value);
-            let configStr = '<!-- @' + name + ' --><div class="config-option-warpper-sub col-12 d-none" parent="' + targetConfig + '" targetValue="' + targetValue + '"><input class="form-check-input config-option-sub" type="checkbox" value="" id="' + name + '" name="' + name + '" autocomplete="off"' + ((value=="true")?' checked="checked"':'') +'><label class="form-check-label text-primary" for="' + name + '">' + diplayName + '</label><p class="text-muted"><i class="fa fa-info-circle"></i> ' + description + '</p></div>';
+            let configStr = '<!-- @' + name + ' -->\n<div class="config-option-warpper-sub col-12 d-none" parent="' + targetConfig + '" targetValue="' + targetValue + '">\n\t<input class="form-check-input config-option-sub" type="checkbox" value="" id="' + name + '" name="' + name + '" autocomplete="off"' + ((value=="true")?' checked="checked"':'') +'>\n\t<label class="form-check-label text-primary" for="' + name + '">' + diplayName + '</label>\n\t<p class="text-muted"><i class="fa fa-info-circle"></i> ' + description + '</p>\n</div>';
             $("#source").text(configStr);
         }
         else if(type=="select")
@@ -137,13 +137,13 @@ function make()
             let argList=arg.split(",");
             console.log(argList);
 
-            let configStr='<!-- @' + name + ' --><div class="config-option-warpper-sub col-12 d-none" parent="' + targetConfig + '" targetValue="' + targetValue + '"><label for="' + name + '" class="text-primary"><i class="fa fa-chevron-down"></i> ' + diplayName + '</label><select class="custom-select config-option-sub" name="' + name + '" autocomplete="off" id="' + name + '" appendId="'+ appendId +'">\n';
+            let configStr='<!-- @' + name + ' -->\n<div class="config-option-warpper-sub col-12 d-none" parent="' + targetConfig + '" targetValue="' + targetValue + '">\n\t<label for="' + name + '" class="text-primary"><i class="fa fa-chevron-down"></i> ' + diplayName + '</label>\n\t<select class="custom-select config-option-sub" name="' + name + '" autocomplete="off" id="' + name + '" appendId="'+ appendId +'">\n';
             for(let i = 0;i<argList.length;i++)
             {
                 
-                configStr = configStr + '<option value="' + argList[i] + '"' + ((i==0)?' selected="selected"':'') +'>' + argList[i] +'</option>\n';
+                configStr = configStr + '\t\t<option value="' + argList[i] + '"' + ((i==0)?' selected="selected"':'') +'>' + argList[i] +'</option>\n';
             }
-            configStr = configStr + '</select><p class="text-muted"><i class="fa fa-info-circle"></i> ' + description + '</p></div>';
+            configStr = configStr + '\t</select>\n\t<p class="text-muted"><i class="fa fa-info-circle"></i> ' + description + '</p>\n</div>';
             $("#source").text(configStr);
         }
     }
